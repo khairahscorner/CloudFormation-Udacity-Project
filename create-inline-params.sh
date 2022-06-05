dae51f563556bfd153c28ddf1681ddcac9f67d64
khairahscorner <airahyusuff@gmail.com>
2022-06-05 16:59:30 +0100

    project 2

---
+++ create-inline-params.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters ParameterKey=InstanceType,ParameterValue=t3.medium ParameterKey=LaunchConfigAMI,ParameterValue=ami-09d56f8956ab235b3 ParameterKey=MyIp,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
