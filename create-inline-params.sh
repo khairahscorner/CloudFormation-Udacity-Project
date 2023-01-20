d89688f725ecb057082e32ea9d0e5d4d752d2cba
khairahscorner <airahyusuff@gmail.com>
2023-01-20 19:28:18 +0000

    updated to launch templates

--- create-inline-params.sh
+++ create-inline-params.sh
@@ -1,1 +1,5 @@
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters ParameterKey=InstanceType,ParameterValue=t3.medium ParameterKey=LaunchConfigAMI,ParameterValue=ami-09d56f8956ab235b3 ParameterKey=MyIp,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+#old
+# aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters ParameterKey=InstanceType,ParameterValue=t3.medium ParameterKey=LaunchConfigAMI,ParameterValue=ami-09d56f8956ab235b3 ParameterKey=MyIp,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
+#with launch template
+aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters ParameterKey=InstanceType,ParameterValue=t3.medium ParameterKey=LaunchTemplateAMI,ParameterValue=ami-09d56f8956ab235b3 ParameterKey=MyIp,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
